88697eb4a29e698933694bfc4f0936d899abe699
yeamanan <me@yeamanan.com>
2016-03-28 21:15:04 +0200

    Rename of addition service

---
+++ addition-service/src/main/kotlin/com/yeamanan/mscalculator/addition/main.kt
@@ -0,0 +1,10 @@
+package com.yeamanan.mscalculator.addition
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter
+
+fun main(args: Array<String>) {
+    val msAddition = SpringApplication(AdditionService::class.java)
+    msAddition.addListeners(ApplicationPidFileWriter("addition-service.pid"))
+    msAddition.run(*args)
+}
