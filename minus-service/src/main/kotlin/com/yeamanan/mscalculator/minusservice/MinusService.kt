47934c2deecc244bfee9ad578b5ca2cb90b4e4fa
yeamanan <me@yeamanan.com>
2016-03-28 20:15:57 +0200

    Add of hystrix and his dashboard

--- minus-service/src/main/kotlin/com/yeamanan/mscalculator/minusservice/MinusService.kt
+++ minus-service/src/main/kotlin/com/yeamanan/mscalculator/minusservice/MinusService.kt
@@ -1,17 +1,21 @@
 package com.yeamanan.mscalculator.minusservice
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Bean
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-//@EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @RestController
 open class MinusService {
 
@@ -22,7 +26,14 @@ open class MinusService {
         log.info("Minus Service Started")
     }
 
-    @RequestMapping("/")
-    fun helloWorld(): String = "Hello World from Minus Service!"
+    @RequestMapping("/{x:-?\\d+},{y:-?\\d+}")
+    @HystrixCommand(groupKey = "calculator", fallbackMethod = "defaultResult")
+    fun minus(@PathVariable("x") x: Int, @PathVariable("y") y: Int): Int {
+        return x - y
+    }
+
+    fun defaultResult(x: Int, y: Int): Int {
+        return 0
+    }
 
 }
